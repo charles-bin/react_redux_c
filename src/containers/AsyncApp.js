567846fc1bac501af9f576ffe45e7454e740b540
Charles Bin <charles.bin@gmail.com>
2017-07-27 00:59:01 +0700

    Moved auth to redux container. Added API requests for a channel's liked playlist.

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -20,6 +20,55 @@ class AsyncApp extends Component {
   componentDidMount() {
     const { dispatch, selectedSubreddit } = this.props
     dispatch(fetchPostsIfNeeded(selectedSubreddit))
+
+    window.gapi.load('client:auth2', loadClient);
+
+    function loadClient() {
+      // Client ID and API key from the Developer Console
+      const CLIENT_ID = '523535459158-jomkfdco6mt9adj7gtt6691pfj4tp4ah.apps.googleusercontent.com';
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+      // Authorization scopes required by the API. If using multiple scopes,
+      // separated them with spaces.
+      const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+      window.gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+      }).then(function () {
+        // Listen for sign-in state changes.
+        //gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        //updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        //authorizeButton.onclick = handleAuthClick;
+        //signoutButton.onclick = handleSignoutClick;
+        var playlistId;
+        window.gapi.client.youtube.channels.list({
+          'part': 'snippet,contentDetails,statistics',
+          'forUsername': 'BBCEarth'
+        }).then(function(response) {
+          let channel = response.result.items[0];
+          console.log('This channel\'s ID is ' + channel.id + '. ' +
+                    'Its title is \'' + channel.snippet.title + ', ' +
+                    'and it has ' + channel.statistics.viewCount + ' views.');
+          console.log('The liked playlist ID is: ' + channel.contentDetails.relatedPlaylists.likes);
+          playlistId = channel.contentDetails.relatedPlaylists.likes;
+
+          window.gapi.client.youtube.playlistItems.list({
+            'maxResults': '25',
+            'part': 'snippet,contentDetails',
+            'playlistId': playlistId
+          }).then(function(response) {
+            var items = response.result.items;
+            items.forEach(function(element) {
+              console.log(element.snippet.title);
+            });
+          });
+        });
+      });
+    }
   }
 
   componentDidUpdate(prevProps) {
